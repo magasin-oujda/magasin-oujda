ed8f32398e7f080f605c599e07d2173e594a1fda
magasin-oujda <mohammedbensalahyounsi@gmail.com>
2024-11-30 13:29:35 +0100

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyAviLwnV_eQ7yU6qP3cAbMTgx7G3sQBwDg",
+    authDomain: "tienda-camisetas-bfaa6.firebaseapp.com",
+    databaseURL: "https://tienda-camisetas-bfaa6-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "tienda-camisetas-bfaa6",
+    storageBucket: "tienda-camisetas-bfaa6.firebasestorage.app",
+    messagingSenderId: "287261715035",
+    appId: "1:287261715035:web:784d4000cc211f72cc6d7e",
+    measurementId: "G-VD308N9GE9"
+};
